6237ca07474312da4252bf42e2e2612be36c7333
madrigal415 <dsmadrigal@gmail.com>
2021-04-01 16:49:32 -0700

    making table responsive and adding viewport info

--- js/publications.js
+++ js/publications.js
@@ -1,13 +1,13 @@
 // File to read in publications from google sheet 
 
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+
 const publicationsURL = `https://docs.google.com/spreadsheets/d/e/2PACX-1vSdMoLdjbXCWBHtdrkEvpgLBx-ouUQtCirElYCaJUSqPsC4WbX4LdmoEWpJUw6q33NIb85mzfcH02VU/pub?gid=2137533580&single=true&output=csv`;
 
 const publications = `../data/dsm-publications - public.csv`
 
 
 var tabulate = function (data,columns) {
-
-    console.log(columns);
 
     var width = document.querySelector('#publications')
         .offsetWidth;
@@ -41,19 +41,43 @@ var tabulate = function (data,columns) {
         .enter()
       .append('td')
         .text(function (d) { return d.value })
-  
+        console.log(data);
+
     return table;
   }
+  
+var makeDivs = function(data){
+  console.log("testing");
+    const pubsDiv = document.querySelector('#publications');
+    let publications = "";
 
+    data.map(x =>{
+      publications += `<div><h4>${x.title}</h4><hr><p>${x.authors}, ${x.journal},${x.year}</p><p><a href="${x.link}">check it out</a></p></div>`;
+    })
 
+    console.log(data);
+
+    pubsDiv.insertAdjacentHTML('beforeend',publications);
+  
+  }
+ 
 
  d3.csv(publications)
   .then(function(data) {
       // data is now whole data set
       // draw chart in here!
+
+      console.log(vw);
       var columns = ["title","authors", "year","journal"];
-      tabulate(data, columns);
-  })
+      if(vw > 600) {
+        console.log("BIG");
+        tabulate(data, columns);
+      }else {
+        console.log("SMALL");
+        makeDivs(data);
+      }
+
+      })
   .catch(function(error){
      // handle error   
   });
